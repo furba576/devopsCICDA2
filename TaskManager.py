c6976846c19ecc1fb943523b4419bde08ddfed42
Furba Tenzing Sherpa <furbatenzingsherpa@Furbas-MacBook-Air.local>
2024-04-12 10:57:48 +0930

    List all task feature added

--- TaskManager.py
+++ TaskManager.py
@@ -7,3 +7,10 @@ class TaskManager:
     def __init__(self):
         self.tasks = []
 
+    def list_tasks(self):
+        if not self.tasks:
+            print("No tasks.")
+        else:
+            for i, task in enumerate(self.tasks):
+                status = "Completed" if task.completed else "Not Completed"
+                print(f"{i+1}. {task.description} - {status}")
