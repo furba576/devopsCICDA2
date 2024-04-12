0283c41f0950182188577a7761bf789bbe9ddf5e
Furba Tenzing Sherpa <furbatenzingsherpa@Furbas-MacBook-Air.local>
2024-04-12 10:52:45 +0930

    Project Initial Commit
    [add] .gitignore file is configured and added
    [add] new file added for simple task manager app

---
+++ TaskManager.py
@@ -0,0 +1,9 @@
+class Task:
+    def __init__(self, description):
+        self.description = description
+        self.completed = False
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
